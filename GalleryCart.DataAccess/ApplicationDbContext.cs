0131bffc8b5984fe77a2d68216301e8229d49305
desamma <lennaqb4@gmail.com>
2025-05-31 19:33:56 +0700

    Init commit

---
+++ GalleryCart.DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using GalleryCart.Models.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GalleryCart.DataAccess
+{
+    public class ApplicationDbContext : IdentityDbContext<User, IdentityRole<Guid>, Guid>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+        }
+    }
+}
---
+++ GalleryCart.Models/Models/User.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GalleryCart.Models.Models
+{
+    public class User : IdentityUser<Guid>
+    {
+        [MaxLength(50)]
+        [Required]
+        public override string UserName { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Date of Birth")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateOnly? UserDOB { get; set; }
+
+        [Display(Name = "Phone Number")]
+        [DataType(DataType.PhoneNumber)]
+        [StringLength(11, ErrorMessage = "The phone number must be at most 11 digits.")]
+        [RegularExpression(@"^\d{1,11}$", ErrorMessage = "The phone number must only contain digits")]
+        public override string? PhoneNumber { get; set; }
+
+        [ValidateNever] 
+        public string? UserAvatar { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime? CreatedDate { get; set; } = DateTime.Now;
+
+        public bool isBanned { get; set; }
+        public bool isArtits { get; set; }
+        public string ProfessionSummary { get; set; } = string.Empty;
+        public string Skills { get; set; } = string.Empty;
+        public string Software { get; set; } = string.Empty;
+        public string ContactInfo { get; set; } = string.Empty;
+        public bool JobLess { get; set; }
+        public bool CommissionStatus { get; set; } // true: open
+    }
+}
